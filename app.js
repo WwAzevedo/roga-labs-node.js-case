0889cbeeead3790fc2f36828c4a2864d75cdcfb4
ww_azevedo <wesley.wazevedo@fco.net.br>
2023-04-27 13:45:22 -0300

    Insert the endpoints routes from pessoas and anotações to app.js

--- app.js
+++ app.js
@@ -2,11 +2,17 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 
+const pessoaRouter = require('./src/routes/pessoasRouter');
+const anotacaoRouter = require('./src/routes/anotacoesRouter');
+
+
 const app = express();
 app.use(bodyParser.json());
 app.use(cors());
 
-
+// Rotas
+app.use(pessoaRouter);
+app.use(anotacaoRouter);
 
 
 app.listen(3000, () => {
