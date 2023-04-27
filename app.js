36831339bffea33e48903092fa7710999350b385
ww_azevedo <wesley.wazevedo@fco.net.br>
2023-04-27 11:51:25 -0300

    Initial node.js/express config

---
+++ app.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+
+const app = express();
+app.use(bodyParser.json());
+app.use(cors());
+
+
+
+
+app.listen(3000, () => {
+  console.log('Server is listening on port 3000');
+});
