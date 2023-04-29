f849ce5262f02c9e6b12bbbf9d00745222d36090
ww_azevedo <wesley.wazevedo@fco.net.br>
2023-04-28 21:12:07 -0300

    Create a middelaware to set CreationDate, UpdateDate on payload before finish the request

---
+++ src/Middelawares/datetimeMiddelawares.js
@@ -0,0 +1,26 @@
+// middleware para adicionar a data de criação
+const setCreationDate = (req, res, next) => {
+    req.body.data_cadastro = new Date();
+    next();
+  };
+  
+// middleware para adicionar a data de atualização
+const setUpdateDate = (req, res, next) => {
+  req.body.data_edicao = new Date();
+  next();
+};
+
+// middleware para impedir que o cliente insira os valores de data_cadastro ou data_edicao
+const checkDateFields = (req, res, next) => {
+  const { data_cadastro, data_edicao } = req.body;
+  if (data_cadastro || data_edicao) {
+    return res.status(400).json({ error: 'Os campos data_cadastro e data_edicao não podem ser atualizados diretamente.' });
+  }
+  next();
+};
+
+module.exports = {
+    setCreationDate,
+    setUpdateDate,
+    checkDateFields,
+}
