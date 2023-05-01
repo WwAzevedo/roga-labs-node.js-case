35e9daf1fdcc1fef091143531029f72746adfcb6
ww_azevedo <wesley.wazevedo@fco.net.br>
2023-05-01 14:55:25 -0300

    Change Postgres Connection

--- src/database/connection.js
+++ src/database/connection.js
@@ -3,11 +3,7 @@ require('dotenv').config();
 
 // Configurações da conexão com o banco de dados
 const pool = new Pool({
-  user: process.env.PG_USER,
-  host: process.env.PG_HOST,
-  database: process.env.PG_DATABASE_NAME,
-  password: process.env.PG_PASSWORD,
-  port: process.env.PG_PORT,
+  connectionString: process.env.PG_HOST,
 });
 
 module.exports = pool;
